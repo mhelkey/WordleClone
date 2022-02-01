59108a3e6c3b8aa61ae6411887239f48b5e853a4
Matthew Helkey <mshelkey@ucdavis.edu>
2022-02-01 15:21:45 -0800

    linear time char pos checking

--- wordle_cli.py
+++ wordle_cli.py
@@ -4,22 +4,24 @@ def verifyWord(string: str) -> bool:
   d = enchant.Dict("en_US")
   return d.check(string) and len(string) == 5
 
-def compareWord(user_string: str, ans_string: str):
-  # O(n^2) to check char correctness
-  answer = [0] * 5 
-  # 0 if char not in ans 
-  # 1 if in ans, but not in correct pos
-  # 2 if in ans and in correct pos
-  for i in range(0,5):
-    if user_string[i:i+1] in ans_string:
-      answer[i] = 1
-    if 
-  return answer
-
 def compareWordLinearly(usr: str, ans: str): # O(2n) time
   # add all char in ans to alphabet arr
+  arr = [0] * 26
+  pos = [0] * 5
+  for i in range(0,5):
+    arr[ord(ans[i]) - 97] += 1
+  for i in range(0,5):
+    if(usr[i] == ans[i]):
+      pos[i] = 2
+      arr[ord(usr[i]) - 97] -= 1
+  for i in range(0,5):
+    if(arr[ord(usr[i]) - 97] > 0):
+      arr[ord(usr[i]) - 97] -= 1
+      pos[i] = 1
+
+  return pos
 def main():
-    answer = compareWord("evift", "fivel")
+    answer = compareWordLinearly("evill", "fivel")
     print(answer)
 
 if __name__ == "__main__":
