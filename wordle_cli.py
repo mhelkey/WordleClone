04c28cbceeded865432d4d662a6d1d13b37fe522
Matthew Helkey <mshelkey@ucdavis.edu>
2022-02-01 12:11:46 -0800

    check word validity in realtime

--- wordle_cli.py
+++ wordle_cli.py
@@ -5,6 +5,7 @@ def verifyWord(string: str) -> bool:
   return d.check(string) and len(string) == 5
 
 def compareWord(user_string: str, ans_string: str):
+  # O(n^2) to check char correctness
   answer = [0] * 5 
   # 0 if char not in ans 
   # 1 if in ans, but not in correct pos
@@ -12,9 +13,11 @@ def compareWord(user_string: str, ans_string: str):
   for i in range(0,5):
     if user_string[i:i+1] in ans_string:
       answer[i] = 1
+    if 
   return answer
 
-
+def compareWordLinearly(usr: str, ans: str): # O(2n) time
+  # add all char in ans to alphabet arr
 def main():
     answer = compareWord("evift", "fivel")
     print(answer)
