4c82fa34945f91857526829153ca9a9f394a7219
Matthew Helkey <mshelkey@ucdavis.edu>
2022-01-31 18:49:44 -0600

    add right letter, wrong pos check

--- wordle_cli.py
+++ wordle_cli.py
@@ -1,9 +1,25 @@
 import enchant
 
-def spellCheck(string: str) -> bool:
+def verifyWord(string: str) -> bool:
   d = enchant.Dict("en_US")
-  return d.check(string)
+  return d.check(string) and len(string) == 5
 
-def lengthCheck(string: str) -> bool:
-  return len(string) == 5
+def compareWord(user_string: str, ans_string: str):
+  answer = [0] * 5 
+  # 0 if char not in ans 
+  # 1 if in ans, but not in correct pos
+  # 2 if in ans and in correct pos
+  for i in range(0,5):
+    if user_string[i:i+1] in ans_string:
+      answer[i] = 1
+  return answer
+
+
+def main():
+    answer = compareWord("evift", "fivel")
+    print(answer)
+
+if __name__ == "__main__":
+    main()
+
 
