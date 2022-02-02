544bee2e6824aba49eb6d4a8e27820cfda313862
Matthew Helkey <mshelkey@ucdavis.edu>
2022-02-02 15:30:21 -0800

    write to a csv

--- generate_words.py
+++ generate_words.py
@@ -1,17 +1,15 @@
 from english_words import english_words_lower_alpha_set
-
+import numpy
 
 def main():
   five_letter = set()
   for i in english_words_lower_alpha_set:
     if(len(i) == 5 and i.isalpha()):
       five_letter.add(i)
-  five_letter = list(five_letter)
-  print(len(five_letter))
-
-  f = open("words.txt", "a")
-  f.writelines('\n'.join(five_letter))
-  f.close()
+  five_letter = numpy.asarray(list(five_letter))
+  print((five_letter))
+  numpy.savetxt('answer.csv', five_letter, delimiter=',', fmt='%s')
+  
 
 if __name__ == '__main__':
   main()
